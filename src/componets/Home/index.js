c1fa220eba68d4c68436f1057bd878d880dee5a4
dhiraj_nayak <dhiraj.nayak@rewoke.in>
2023-01-26 14:54:48 +0530

    Bootstrap removed and tailwind added

--- src/componets/Home/index.js
+++ src/componets/Home/index.js
@@ -3,19 +3,22 @@ import Skills from '../Skills';
 import featuredImage from '../../assets/images/featued_image.png';
 export default function Home() {
   return (
-    <div className="mt-4 pt-4" >
-       <div className="d-flex m-4 p-4">
-       <div className="col-4 pt-4 pl-2" style={{  justifyContent:'center'}}>
-      
-          <h1 style={{alignSlef:'end', paddingLeft:'40%', alignSelf:'center'}} className="pl-4"> Hii!  </h1>
+    <div className="mt-4 pt-4 " >
+       <div className="d-flex  row ">
+       <div className="col-lg-4 pt-4  pl-4 col-sm-12 order-first" >
+             
+             <div className=" m-5">
+
+            
+          <h1 style={{alignSlef:'end'}} className="pl-4"> Hi I'm Dhiraj Kumar Nayak  </h1>
           
-          <div className="text-center">
-          <h2 style={{alignSlef:'start', color:'red'}}> I'm Dhiraj Kumar Nayak</h2>
+         
+          <h2 style={{alignSlef:'start', color:'red'}}> </h2>
           <h4 style={{ color:'orange'}}>Web & Mobile App Developer</h4>
           <h5 style={{color:'blue'}}>#React #React-Native #Vue #Node</h5>
           
           <button type="button" className="btn text-center btn-info">Hire Me</button>
-          </div>
+          
           
 
           <i className="bi bi-facebook"></i>
@@ -24,11 +27,13 @@ export default function Home() {
 
          <h5>8249428963</h5>
          <Skills/> */}
+         </div>
        </div> 
-       <div className="col-8"  >
+       <div className="col-lg-8   order-last order-md-1 "  >
             <img src={featuredImage} width="100%" height="auto"/>
         </div>    
     </div>
+
     </div>
   );
 }
