7a7cf50f4a32729b78560d0b89eb3ea789304dde
dhiraj_nayak <dhiraj.nayak@rewoke.in>
2023-01-29 21:22:41 +0530

    Responsiveness to hero section added

--- src/componets/Home/index.js
+++ src/componets/Home/index.js
@@ -1,23 +1,23 @@
 import React from "react";
-import Skills from '../Skills';
 import featuredImage from '../../assets/images/featued_image.png';
+import Project from "../project";
 export default function Home() {
   return (
     <div className="mt-4 pt-4 " >
-       <div className="font-mono  grid grid-cols-3  sm:grid-col-3 ">
+       <div className="font-mono  grid grid-cols-1  sm:grid-cols-3">
        <div className="flex justify-center content-center	items-center" >
              
              <div className=" m-5">
 
             
-          <h1 style={{alignSlef:'end'}} className="text-xl"> Hi I'm Dhiraj Kumar Nayak  </h1>
+          <h1 style={{alignSlef:'end'}} className="text-xl mb-2"> Hi I'm Dhiraj Kumar Nayak  </h1>
           
          
           <h2 style={{alignSlef:'start', color:'red'}}> </h2>
-          <h4 style={{ color:'orange'}} >Full stack developer</h4>
-          <p style={{color:'blue'}}>#React #React-Native #Vue #Node #Express</p>
+          <h4 style={{ color:'orange'}} className="mb-2 text-lg" >Full Stack Developer</h4>
+          <p style={{color:'blue'}} className="text-sm">#React #React-Native #Vue #Node #Express</p>
           
-          <button type="button" className=" bg-cyan-300 p-2">Hire Me</button>
+          <button type="button" className=" bg-cyan-300 hover:bg-cyan-500  p-2 rounded mt-4">Hire Me</button>
           
           
 
@@ -29,10 +29,12 @@ export default function Home() {
          <Skills/> */}
          </div>
        </div> 
-       <div className=" lg:col-span-2"  >
+       <div className="sm:col-span-2"  >
             <img src={featuredImage} width="100%" height="auto"/>
         </div>    
     </div>
+
+     <Project/>
 
     </div>
   );
---
+++ src/componets/project/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Project (){
+    return(
+        <>
+           <div className="container">
+                <h1 className="text-xl"> Projects</h1>  
+
+            </div>
+        </>
+    )
+}
+
+
+export default Project;
